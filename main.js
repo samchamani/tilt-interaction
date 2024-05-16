7bdeed876a030bf4ce013d07cbe1e0d4c88e407b
Sam Chamani <sam.chamani@live.de>
2024-05-16 23:43:01 +0200

    permission check and clean up

--- main.js
+++ main.js
@@ -1,26 +1,29 @@
 import "./style.css";
 
-// Permission check
-if (DeviceOrientationEvent.requestPermission) {
-  (async () => {
-    const granted = await DeviceOrientationEvent.requestPermission();
-    if (granted === "granted") {
 
-      // add listener
-      const word = document.querySelector(".tilting-word");
-      window.addEventListener("deviceorientation",handleDeviceOrientation,false);
 
-      let count = 0;
-      function handleDeviceOrientation(event) {
-        count += 1;
-        // use count to reduce the changes to the DOM
-        if (count % 5 === 0) {
+// add click listener to tilting word
+const word = document.querySelector(".tilting-word");
 
-          // apply styles based on device orientation angles
-          word.style.filter = `drop-shadow(${-event.gamma}px ${-event.beta}px 2px salmon)`;
-          word.style.transform = `skewX(${event.gamma / 3}deg)`;
-        }
-      }
-    }
-  })();
+word.addEventListener("click", () => {
+  // Permission check on click and then removal of click listener
+  DeviceOrientationEvent["requestPermission"]();
+  word.innerHTML = "TILT ME"
+  word.removeEventListener("click")
+})
+
+// add device orientation listener to window and with handleDeviceOrietantion
+// as event handler
+window.addEventListener("deviceorientation", handleDeviceOrientation, false);
+
+// use count to reduce the changes to the DOM
+let count = 0;
+function handleDeviceOrientation(event) {
+  count += 1;
+  if (count % 5 === 0) {
+
+    // apply styles based on device orientation angles
+    word.style.filter = `drop-shadow(${-event.gamma}px ${-event.beta}px 6px salmon)`;
+    word.style.transform = `skewX(${event.gamma / 3}deg)`;
+  }
 }
