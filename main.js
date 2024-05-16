73ea272e72493d068b77472f1879967504c940bd
Sam Chamani <sam.chamani@live.de>
2024-05-16 22:47:43 +0200

    tilting animation based on device orientation

--- main.js
+++ main.js
@@ -1,61 +1,49 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
+const word = document.querySelector(".tilting-word")
+window.addEventListener('deviceorientation', handleDeviceOrientation, false)
 
-  <div class="tilt-container">
-    <button class="request-button">Request</button>
-    <div class="tilting-word" style="filter: drop-shadow(42px 42px 2px salmon)">
-      TILT ME
-    </div>
-  </div>
+let count = 0
+function handleDeviceOrientation(event) {
+  count += 1;
+  if (count % 5 === 0){
+    word.style.filter = `drop-shadow(${-event.gamma}px ${-event.beta}px 2px salmon)`
+    word.style.transform = `skewX(${event.gamma/3}deg)`
+    console.log(word.style.transform)
+  }
+}
 
-`
-// // Check if the browser supports the Permissions API
-// if (navigator.permissions && navigator.permissions.query) {
-//   // Request permission for device orientation
-//   navigator.permissions.query({ name: 'accelerometer' }).then(permissionStatus => {
-//       if (permissionStatus.state === 'granted') {
-//           // Permission already granted
-//           console.log('Permission for device orientation granted');
-//           // Set up event listener for deviceorientation
-//           window.addEventListener('deviceorientation', handleOrientation, false);
-//       } else if (permissionStatus.state === 'prompt') {
-//           // Permission prompt needed
-//           console.log('Permission prompt needed for device orientation');
-//           // Handle permission prompt if necessary
-//           // For example, you might prompt the user to allow device orientation access
-//       } else {
-//           // Permission denied
-//           console.log('Permission for device orientation denied');
-//           // Handle denied permission if necessary
-//       }
-//   }).catch(error => {
-//       // Error handling
-//       console.error('Error requesting permission:', error);
-//   });
-// } else {
-//   // Permissions API not supported
-//   console.log('Permissions API not supported');
-//   // Handle lack of support if necessary
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function endlessLoop() {
+//   setTimeout(doAnimation, 100)
+// }
+// let x = 0
+// let y = 0
+
+// function doAnimation(){
+//   word.style.filter = `drop-shadow(${x}px ${40 + y/10}px 2px salmon)`
+
+//   // x++
+//   // y++
+//   console.log(x,y, word.style.filter)
+//   endlessLoop()
 // }
 
-document.querySelector('.request-button').addEventListener("click", handleClick)
+// endlessLoop()
 
-function handleClick(){
-  DeviceOrientationEvent['requestPermission']()
-}
-
-if (window.DeviceOrientationEvent){
-  DeviceOrientationEvent['requestPermission']()
-}
-
-
-window.addEventListener('deviceorientation', handleDeviceOrientation)
-
-function handleDeviceOrientation(event) {
-  console.log(event)
-}
-
-
-// filter: drop-shadow(0px 0px 2px salmon);
+// filter: drop-shadow(0px 0px 2px salmon); style="filter: drop-shadow(42px 42px 2px salmon)"
 // transform: rotate3d(0, 0, 0, 45deg);
+
+{/* <button class="request-button">Request</button> */}
